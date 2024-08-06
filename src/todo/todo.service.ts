eb1749dc3e8b78680d99c35a31d1427decb3e8f3
Xusanjon <xusanmurodov907@gmail.com>
2024-08-07 02:28:12 +0500

    graphql

---
+++ src/todo/todo.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTodoInput } from './dto/create-todo.input';
+import { UpdateTodoInput } from './dto/update-todo.input';
+import { Todo } from "./entities/todo.entity"
+
+@Injectable()
+export class TodoService {
+  private todo:Todo[] = []
+  create(createTodoInput: CreateTodoInput) {
+    console.log(createTodoInput);
+    // const 
+    
+    return 'This action adds a new todo';
+  }
+
+  findAll() {
+    return `This action returns all todo`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} todo`;
+  }
+
+  update(id: number, updateTodoInput: UpdateTodoInput) {
+    return `This action updates a #${id} todo`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} todo`;
+  }
+}
